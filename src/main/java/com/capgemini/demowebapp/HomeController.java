bed3e974d65d5b2f66987e435d6d59451ed8f5db
mamudsamiul <samiulmamud1995@gmail.com>
2020-11-29 01:35:12 +0530

    (Samiul Mamud) Refactored: Model An view is introduced

--- src/main/java/com/capgemini/demowebapp/HomeController.java
+++ src/main/java/com/capgemini/demowebapp/HomeController.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller // Controller to handle request localhost:8080
 public class HomeController {
 	@RequestMapping("home") // request mapping
 //	@ResponseBody // it is used to send data
-	public String home(@RequestParam("name") String myName, HttpSession session) {
-		System.out.println("hello "+myName);
-		session.setAttribute("name", myName);
-		return "home"; //page name
+	public ModelAndView home(@RequestParam("name") String myName) {
+		ModelAndView mv=new ModelAndView();
+		mv.addObject("name",myName);
+		mv.setViewName("home");
+		return mv; //page name
 	}
 
 }
